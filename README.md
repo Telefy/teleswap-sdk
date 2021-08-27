8e108b58111d490f154af9a211d6b888a56e8680
lokisanhitleson <lokesh603@gmail.com>
2021-08-27 13:32:45 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Teleswap SDK
+
+For more information, contact https://mazelon.com
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x1eD4f76F9e4AA7a89a0D8182264dE423f1791825'
+
+export const INIT_CODE_HASH = '0x21b0320a6a5c13fd3123af41014427675152b5eaed8ec28f49104c3648faf200'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
